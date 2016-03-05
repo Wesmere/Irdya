9cfdbddf5193ba825d3fde59bc19b5fa4ac1f108
Fabian Müller <fendrin@gmx.de>
2016-03-05 01:50:38 +0100

    Silly change of the package type.

--- manifest.lua
+++ manifest.lua
@@ -1,7 +1,7 @@
 {
     id = "Irdya",
     name = "Irdya",
-    type = "good question",
+    type = "good question, maybe game_core?",
     description = "Irdya is a Wesmere game server",
     rank = -10
 }
